8bb60ba9ac5990ae084e7034da63116fd461b70b
Abid Ahmed <ahmedabid3409@gmail.com>
2023-02-13 20:55:42 +0600

    Update Driver.cs

--- Driver.cs
+++ Driver.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]      float steerSpeed=0.5f;
+  [SerializeField]      float steerSpeed=0.5f;
   [SerializeField] float moveSpeed=0.01f;
+
+  
+
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -18,5 +21,12 @@ public class Driver : MonoBehaviour
         float moveAmount=Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+      moveSpeed-=10f;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+      if(other.tag=="Boost")
+      moveSpeed+=30f;
     }
 }
